d86e1b1621e8d56854a4d8761807e9121c6a59fe
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-10-28 20:12:28 +0100

    feat: initial commit. add x71c9 tempesta package.

--- default.nix
+++ default.nix
@@ -9,12 +9,9 @@
 { pkgs ? import <nixpkgs> { } }:
 
 {
-  # The `lib`, `modules`, and `overlays` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
-  overlays = import ./overlays; # nixpkgs overlays
+  lib = import ./lib { inherit pkgs; };
+  modules = import ./modules;
+  overlays = import ./overlays;
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  tempesta = pkgs.callPackage ./pkgs/tempesta { };
 }

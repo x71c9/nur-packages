cf9a3087b9ade9383a4aa4b4777339b6c2a287b4
x71c9 <108585118+x71c9@users.noreply.github.com>
2026-06-24 14:59:41 +0000

    dogma: 1.8.0

--- pkgs/dogma/default.nix
+++ pkgs/dogma/default.nix
@@ -4,14 +4,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "dogma";
-  version = "1.7.0"; # without "v"
+  version = "1.8.0"; # without "v"
 
   # Pin the source to an immutable tag/commit
   src = fetchFromGitHub {
     owner = "x71c9";
     repo = "dogma";
     rev = "v${version}";
-    hash = "sha256-ca1Aam1CJWDcyrv1Qu3++/DqV6VYpLpkaQztYckhdDc=";
+    hash = "sha256-vTQqzRGR8L5Ez3Y66B3a6Ob9FLJ0OiaurV+cstUQWEg=";
   };
 
   # Vendor Cargo dependencies from the committed lockfile.
